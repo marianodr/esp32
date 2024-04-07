f1a1db78cdca1329fff10f36c9232750c36c26d9
MarianoR <marianodrodriguez03@gmail.com>
2024-04-06 22:44:05 -0300

    Funciona en local

--- boot.py
+++ boot.py
@@ -1,1 +1,16 @@
-# boot.py -- run on boot-up
+from settings import SSID, password
+
+
+def do_connect():
+   import network
+   wlan = network.WLAN(network.STA_IF)
+   wlan.active(True)
+   if not wlan.isconnected():
+       print('connecting to network...')
+       wlan.connect(SSID, password)
+       while not wlan.isconnected():
+           pass
+   print('network config:', wlan.ifconfig())
+
+
+do_connect()
--- main.py
+++ main.py
@@ -1,59 +1,66 @@
-# (C) Copyright Peter Hinch 2017-2019.
-# Released under the MIT licence.
+from machine import Pin, Timer, unique_id
+import dht
+import time
+import json
+from collections import OrderedDict
+from settings import SERVIDOR_MQTT
+from umqtt.robust import MQTTClient
 
-# This demo publishes to topic "result" and also subscribes to that topic.
-# This demonstrates bidirectional TLS communication.
-# You can also run the following on a PC to verify:
-# mosquitto_sub -h test.mosquitto.org -t result
-# To get mosquitto_sub to use a secure connection use this, offered by @gmrza:
-# mosquitto_sub -h <my local mosquitto server> -t result -u <username> -P <password> -p 8883
 
-# Public brokers https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
+CLIENT_ID = 'EXO'
 
-# red LED: ON == WiFi fail
-# green LED heartbeat: demonstrates scheduler is running.
 
-from mqtt_as import MQTTClient
-from mqtt_local import config
-import uasyncio as asyncio
+mqtt = MQTTClient(CLIENT_ID,
+                 SERVIDOR_MQTT,
+                 port=1883,
+                 keepalive=10,
+                 ssl=False)
 
-SERVER = config['server']
 
-def sub_cb(topic, msg, retained):
-    c, r = [int(x) for x in msg.decode().split(' ')]
-    print('Topic = {} Count = {} Retransmissions = {} Retained = {}'.format(topic.decode(), c, r, retained))
+led = Pin(2, Pin.OUT)
+d = dht.DHT22(Pin(25))
+contador = 0
 
-async def wifi_han(state):
-    print('Wifi is ', 'up' if state else 'down')
-    await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
-async def conn_han(client):
-    await client.subscribe('result', 1)
+def heartbeat():
+   global contador
 
-async def main(client):
-    await client.connect()
-    n = 0
-    await asyncio.sleep(2)  # Give broker time
-    while True:
-        print('publish', n)
-        # If WiFi is down the following will pause for the duration.
-        await client.publish('result', '{} {}'.format(n, client.REPUB_COUNT), qos = 1)
-        n += 1
-        await asyncio.sleep(20)  # Broker is slow
 
-# Define configuration
-config['subs_cb'] = sub_cb
-config['server'] = SERVER
-config['connect_coro'] = conn_han
-config['wifi_coro'] = wifi_han
-config['ssl'] = False
+   if contador > 5:
+       pulsos.deinit()
+       contador = 0
+       return
+  
+   led.value(not led.value())
+   contador += 1
 
-# Set up client
-MQTTClient.DEBUG = True  # Optional
-client = MQTTClient(config)
-try:
-    asyncio.run(main(client))
-finally:
-    client.close()
-    asyncio.new_event_loop()
+def transmitir(pin):
+   print("publicando")
+   mqtt.connect()
+   mqtt.publish(f"iot2024/{CLIENT_ID}",datos)
+   mqtt.disconnect()
+   pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
+
+
+publicar = Timer(0)
+publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+pulsos = Timer(1)
+
+
+while True:
+   try:
+       d.measure()
+       temperatura = d.temperature()
+       humedad = d.humidity()
+      
+       datos = json.dumps(OrderedDict([
+           ('temperatura',temperatura),
+           ('humedad',humedad)
+       ]))
+       print(datos)
+
+
+   except OSError as e:
+       print("sin sensor")
+  
+   time.sleep(5)
